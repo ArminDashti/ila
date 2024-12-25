a0664543e591699c717444a588cf0bf34bde6350
ArminDashti <arminonline71@gmail.com>
2024-12-25 23:04:33 +0330

    Update

--- datasets/minari_env.py
+++ datasets/minari_env.py
@@ -1,15 +1,14 @@
 import minari
 import numpy as np
 
-
 class Minari:
     def __init__(self, dataset_id, include_keys=None, normalize_rewards=True):
         self.dataset_id = dataset_id
         self.include_keys = include_keys or [
-            'observations', 'actions', 'rewards', 'terminations', 'truncations',
-            'next_observations', 'next_actions', 'steps', 'next_steps', 'prev_steps',
-            'prev_observations', 'prev_actions', 'prev_rewards',
-            'prev_terminations', 'prev_truncations']
+            'states', 'actions', 'rewards', 'termins', 'truncs',
+            'next_states', 'next_actions', 'steps', 'next_steps', 'prev_steps',
+            'prev_states', 'prev_actions', 'prev_rewards',
+            'prev_termins', 'prev_truncs']
         self.normalize_rewards = normalize_rewards
         self.dataset = minari.load_dataset(dataset_id, download=True)
         self.processed_data = {key: [] for key in self.include_keys}
@@ -18,10 +17,12 @@ class Minari:
         if key in self.include_keys:
             self.processed_data[key].append(value)
 
-    def _get_previous_value(self, values, index, default):
+    @staticmethod
+    def _get_previous_value(values, index, default):
         return values[index - 1] if index > 0 else default
 
-    def _normalize_rewards(self, rewards):
+    @staticmethod
+    def _normalize_rewards(rewards):
         rewards = np.array(rewards)
         if rewards.size == 0:
             return rewards
@@ -31,33 +32,33 @@ class Minari:
         episode_length = len(episode.observations)
 
         for step_index in range(min(100, episode_length - 1)):
-            self._append_to_processed_data('observations', episode.observations[step_index])
+            self._append_to_processed_data('states', episode.observations[step_index])
             self._append_to_processed_data('actions', episode.actions[step_index])
             self._append_to_processed_data('rewards', episode.rewards[step_index])
-            self._append_to_processed_data('terminations', episode.terminations[step_index])
-            self._append_to_processed_data('truncations', episode.truncations[step_index])
-            self._append_to_processed_data('next_observations', episode.observations[step_index + 1])
+            self._append_to_processed_data('termins', episode.terminations[step_index])
+            self._append_to_processed_data('truncs', episode.truncations[step_index])
+            self._append_to_processed_data('next_states', episode.observations[step_index + 1])
 
             next_action = episode.actions[step_index + 1] if step_index + 1 < episode_length else np.zeros_like(episode.actions[step_index])
             self._append_to_processed_data('next_actions', next_action)
             self._append_to_processed_data('steps', step_index + 1)
 
-            previous_observation = self._get_previous_value(episode.observations, step_index, np.zeros_like(episode.observations[step_index]))
-            self._append_to_processed_data('prev_observations', previous_observation)
+            prev_state = self._get_previous_value(episode.observations, step_index, np.zeros_like(episode.observations[step_index]))
+            self._append_to_processed_data('prev_states', prev_state)
 
-            previous_action = self._get_previous_value(episode.actions, step_index, np.zeros_like(episode.actions[step_index]))
-            self._append_to_processed_data('prev_actions', previous_action)
+            prev_action = self._get_previous_value(episode.actions, step_index, np.zeros_like(episode.actions[step_index]))
+            self._append_to_processed_data('prev_actions', prev_action)
 
-            previous_reward = self._get_previous_value(episode.rewards, step_index, 0.0)
+            prev_reward = self._get_previous_value(episode.rewards, step_index, 0.0)
             if self.normalize_rewards:
-                previous_reward = self._normalize_rewards([previous_reward])[0]
-            self._append_to_processed_data('prev_rewards', previous_reward)
+                prev_reward = self._normalize_rewards([prev_reward])[0]
+            self._append_to_processed_data('prev_rewards', prev_reward)
 
-            previous_termination = self._get_previous_value(episode.terminations, step_index, 0)
-            self._append_to_processed_data('prev_terminations', previous_termination)
+            prev_termin = self._get_previous_value(episode.terminations, step_index, 0)
+            self._append_to_processed_data('prev_termins', prev_termin)
 
-            previous_truncation = self._get_previous_value(episode.truncations, step_index, 0)
-            self._append_to_processed_data('prev_truncations', previous_truncation)
+            prev_trunc = self._get_previous_value(episode.truncations, step_index, 0)
+            self._append_to_processed_data('prev_truncs', prev_trunc)
 
             next_step = step_index + 1 if step_index + 1 < episode_length else None
             prev_step = step_index if step_index > 0 else None
@@ -75,9 +76,9 @@ class Minari:
         if 'rewards' in self.include_keys and self.normalize_rewards:
             self.processed_data['rewards'] = self._normalize_rewards(self.processed_data['rewards'])
 
-        if 'terminations' in self.include_keys and 'truncations' in self.include_keys:
+        if 'termins' in self.include_keys and 'truncs' in self.include_keys:
             self.processed_data['dones'] = np.logical_or(
-                self.processed_data['terminations'], self.processed_data['truncations']
+                self.processed_data['termins'], self.processed_data['truncs']
             ).astype(int)
 
         self.processed_data = {key: np.array(value) for key, value in self.processed_data.items()}
@@ -88,6 +89,6 @@ class Minari:
         return self.dataset.recover_environment()
 
 if __name__ == "__main__":
-    minari_ = Minari('D4RL/door/expert-v2')
-    ds = minari_.download_processed()
-    print(ds.keys())
+    minari_instance = Minari('D4RL/door/expert-v2')
+    processed_data = minari_instance.download_processed()
+    print(processed_data['prev_states'][1] == processed_data['states'][0])
